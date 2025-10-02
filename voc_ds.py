557758d942848cb8c3779bbf895c6e731a5cf421
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-10-02 20:45:11 +0700

    update

---
+++ voc_ds.py
@@ -0,0 +1,11 @@
+from torchvision.datasets import VOCDetection
+
+class VOCDataset(VOCDetection):
+  def __init__(self):
+    super().__init__(root, year , image_set=. download=)
+
+  def __int__(self):
+    pass
+
+  def __getitem__(self, item):
+    pass
