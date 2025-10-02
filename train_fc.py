caf531b8d1444e7da69d37ba244b9f049ec9f268
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2025-10-02 20:10:39 +0700

    update

--- train_fc.py
+++ train_fc.py
@@ -2,7 +2,7 @@ from torchvision.datasets import VOCDetection
 
 
 def train():
-  train_dataset = VOCDetection(root  ="my_pascal_voc", year = "2012", image_set = 'train', download = True)
+  train_dataset = VOCDetection(root  ="my_pascal_voc", year = "2012", image_set = 'train', download = False)
   image , label = train_dataset[1000]
   print(label)
   print(image)
